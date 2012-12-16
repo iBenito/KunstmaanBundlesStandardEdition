57dc74dac1ed846987b797f1ab5aa3c389d4a4f5
Alex Fuckert <alex@fuckert.de>
2012-12-16 14:19:41 +0100

    Forgot ImageExtension.php

---
+++ src/Zizoo/BoatBundle/Twig/ImageExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Zizoo\BoatBundle\Twig;
+
+
+class ImageExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'imageWebPath' => new \Twig_Filter_Method($this, 'webPath'),
+        );
+    }
+
+    public function webPath($image)
+    {
+        return 'images/boats/'.$image->getBoat()->getId().'/'.$image->getPath();
+    }
+
+    public function getName()
+    {
+        return 'acme_extension';
+    }
+}
+?>
