41346a17d75e7fef2c26b54810ae8646bd02df1c
Alex Fuckert <alex@fuckert.de>
2013-01-07 16:59:43 +0100

    FB

--- src/Zizoo/UserBundle/Controller/RegistrationController.php
+++ src/Zizoo/UserBundle/Controller/RegistrationController.php
@@ -127,6 +127,7 @@ class RegistrationController extends Controller
                 
             }
         }
+        $router = $this->get("router");
         $fbAppId    = $this->container->getParameter('zizoo_user.facebook.app_id');
         $fbRedirect = urlencode($router->generate('register_facebook', null, true));
         return $this->render('ZizooUserBundle:Registration:register.html.twig', array('form' => $form->createView(), 'unconfirmed_user' => null, 'unconfirmed_email' => false, 'unconfirmed_username' => false, 'facebook_app_id' => $fbAppId, 'facebook_redirect' => $fbRedirect));
@@ -272,6 +273,7 @@ class RegistrationController extends Controller
             if ($code==null || $state==null){
                 die("error");
             }
+            $router = $this->get("router");
             if ($_SESSION['fb_state'] === $state){
                 $token_url = "https://graph.facebook.com/oauth/access_token?"
                             . "client_id=" . $fbAppId . "&redirect_uri=" . urlencode($router->generate('register_facebook_confirm', null, true))
