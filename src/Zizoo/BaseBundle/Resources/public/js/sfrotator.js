e50bc93715a5c52f64ccd2bf469ec2440d93aaf6
imiletic <imiletic@gmail.com>
2013-07-15 02:30:58 +0200

    Synced with Sasa Foric.

--- src/Zizoo/BaseBundle/Resources/public/js/sfrotator.js
+++ src/Zizoo/BaseBundle/Resources/public/js/sfrotator.js
@@ -22,40 +22,40 @@
                 // Variables
                 var option            = options,
                     rotator            = $(this),
-                    rotators        = rotator.find('.rotators'),
-                    button            = rotator.find('.arrow'),
-                    links            = $('.links[data-rotator="' + rotator.attr('id') + '"]');
+                    rotators        = rotator.find(".rotators"),
+                    button            = rotator.find(".arrow"),
+                    links            = $(".links[data-rotator='" + rotator.attr("id") + "']");
 
                 // Start rotator if more than one children and if rotator is visible
-                if (rotators.children().length > 1 && rotator.is(':visible')) {
+                if (rotators.children().length > 1 && rotator.is(":visible")) {
 
                     rotators.children().hide();
-                    rotators.children('li:first-child').show();
-                    rotators.children('li:first-child').addClass('current');
+                    rotators.children("li:first-child").show();
+                    rotators.children("li:first-child").addClass("current");
 
-                    links.children('li:first-child').addClass('current');
+                    links.children("li:first-child").addClass("current");
 
                     var timeOut;
                     function rotate() {
 
-                        var currentLink     = links.children('.current'),
-                            currentRotate    = rotators.children('.current'),
+                        var currentLink     = links.children(".current"),
+                            currentRotate    = rotators.children(".current"),
 
                             nextLink        = currentLink.next(),
                             nextRotate        = currentRotate.next();
 
                         timeOut  = setTimeout(function() {
                             if (nextRotate.length === 0) {
-                                nextLink    = links.children('li:first-child');
-                                nextRotate    = rotators.children('li:first-child');
+                                nextLink    = links.children("li:first-child");
+                                nextRotate    = rotators.children("li:first-child");
                             }
 
-                            currentLink.removeClass('current');
-                            currentRotate.removeClass('current');
+                            currentLink.removeClass("current");
+                            currentRotate.removeClass("current");
                             currentRotate.fadeOut(option.speed);
 
-                            nextLink.addClass('current');
-                            nextRotate.addClass('current');
+                            nextLink.addClass("current");
+                            nextRotate.addClass("current");
                             nextRotate.fadeIn(option.speed);
 
                             rotate();
@@ -65,8 +65,8 @@
                     // Previous and next functions
                     button.click(function() {
 
-                        var currentLink     = links.children('.current'),
-                            currentRotate    = rotators.children('.current'),
+                        var currentLink     = links.children(".current"),
+                            currentRotate    = rotators.children(".current"),
 
                             prevLink        = currentLink.prev(),
                             prevRotate        = currentRotate.prev(),
@@ -75,32 +75,32 @@
                             nextRotate        = currentRotate.next();
 
                         if (nextRotate.length === 0) {
-                            nextLink    = links.children('li:first-child');
-                            nextRotate    = rotators.children('li:first-child');
+                            nextLink    = links.children("li:first-child");
+                            nextRotate    = rotators.children("li:first-child");
                         }
 
                         if (prevRotate.length === 0) {
-                            prevLink    = links.children('li:last-child');
-                            prevRotate    = rotators.children('li:last-child');
+                            prevLink    = links.children("li:last-child");
+                            prevRotate    = rotators.children("li:last-child");
                         }
 
-                        if ($(this).is('.prev')) {
-                            currentLink.removeClass('current');
-                            currentRotate.removeClass('current');
+                        if ($(this).is(".prev")) {
+                            currentLink.removeClass("current");
+                            currentRotate.removeClass("current");
                             currentRotate.stop(true,true).fadeOut(option.speed);
 
-                            prevLink.addClass('current');
-                            prevRotate.addClass('current');
+                            prevLink.addClass("current");
+                            prevRotate.addClass("current");
                             prevRotate.fadeIn(option.speed);
                         }
 
-                        if ($(this).is('.next')) {
-                            currentLink.removeClass('current');
-                            currentRotate.removeClass('current');
+                        if ($(this).is(".next")) {
+                            currentLink.removeClass("current");
+                            currentRotate.removeClass("current");
                             currentRotate.stop(true,true).fadeOut(option.speed);
 
-                            nextLink.addClass('current');
-                            nextRotate.addClass('current');
+                            nextLink.addClass("current");
+                            nextRotate.addClass("current");
                             nextRotate.fadeIn(option.speed);
                         }
 
@@ -115,10 +115,10 @@
                     });
 
                     // Links functions to rotator
-                    links.find('a').click(function() {
-                        rotators.children().eq($(this).parent().index()).fadeIn(option.speed).addClass('current')
-                            .siblings().removeClass('current').stop(true,true).fadeOut(option.speed);
-                            $(this).parent().addClass('current').siblings().removeClass('current');
+                    links.find("a").click(function() {
+                        rotators.children().eq($(this).parent().index()).fadeIn(option.speed).addClass("current")
+                            .siblings().removeClass("current").stop(true,true).fadeOut(option.speed);
+                            $(this).parent().addClass("current").siblings().removeClass("current");
 
                             clearTimeout(timeOut);
                             timeOut = setTimeout(function() {
