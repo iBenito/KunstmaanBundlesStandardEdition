c47400622abbf04c21bb22ee233ef4d7c39cd7b3
Alex Fuckert <alexf83@gmail.com>
2013-07-17 17:41:56 +0200

    Reintroduced responsive dialogs

--- src/Zizoo/BaseBundle/Resources/public/js/required.js
+++ src/Zizoo/BaseBundle/Resources/public/js/required.js
@@ -479,14 +479,56 @@ $(document).ready(function() {
     * Function
     */
 
-   $("#details .options select").change(function() {
-       if ($(this).val() != "options") {
-           $("#details .payment-options").show();
-           $("#details .payment-options #" + $(this).val()).show().siblings().hide();
-       } else {
-           $("#details .payment-options").hide();
-           $("#details .payment-options > section").hide();
-       }
-   });
+//   $("#details .options select").change(function() {
+//       if ($(this).val() != "options") {
+//           $("#details .payment-options").show();
+//           $("#details .payment-options #" + $(this).val()).show().siblings().hide();
+//       } else {
+//           $("#details .payment-options").hide();
+//           $("#details .payment-options > section").hide();
+//       }
+//   });
+
+
+ /* ==========================================================================
+   jQuery responsive dialog
+   ========================================================================== */
+    
+    // on window resize run function
+    $(window).resize(function () {
+        fluidDialog();
+    });
+
+    // catch dialog if opened within a viewport smaller than the dialog width
+    $(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+        fluidDialog();
+    });
+
+    function fluidDialog() {
+        var $visible = $(".ui-dialog:visible");
+        // each open dialog
+        $visible.each(function () {
+            var $this = $(this);
+            var dialog = $this.find(".ui-dialog-content").data("dialog");
+            // if fluid option == true
+            if (dialog.options.fluid) {
+                var wWidth = $(window).width();
+                // check window width against dialog width
+                if (wWidth < dialog.options.maxWidth + 50) {
+                    // keep dialog from filling entire screen
+                    $this.css("max-width", "90%");
+                } else {
+                    // fix maxWidth bug
+                    $this.css("max-width", dialog.options.maxWidth);
+                }
+                //reposition dialog
+                dialog.option("position", dialog.options.position);
+            }
+        });
+
+    }
+
 
 });
+
+
