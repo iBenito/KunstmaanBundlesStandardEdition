eb587615a25b5f11a1f691fe1748f274a262c2ac
vbenitogo <vbenitogo@gmail.com>
2013-11-06 10:50:13 +0100

    Several translations

--- src/Zizoo/AddressBundle/Form/Type/AddressType.php
+++ src/Zizoo/AddressBundle/Form/Type/AddressType.php
@@ -14,37 +14,36 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('address_line_1', 'text', array('label' => array( 'value' => 'zizoo_address.label.address_line1',
-                                                                    'class' => 'location'),
-                                                    'property_path' => 'addressLine1'))
-            ->add('address_line_2', 'text', array(  'label' => array( 'value' => 'zizoo_address.label.address_line2',
-                                                                        'class' => 'location'),
-                                                    'property_path' => 'addressLine2',
-                                                    'required'  => false))
-            ->add('postcode', 'text', array('label' => array( 'value' => 'zizoo_address.label.postcode',
-                                                                'class' => 'postal_code'),
-                                            'property_path' => 'postcode'))
-            ->add('locality', 'text', array('label' => array( 'value' => 'zizoo_address.label.locality',
-                                                                    'class' => 'location'),
-                                            'property_path' => 'locality'))
+            ->add('address_line_1', 'text', array('label' => array('value' => 'zizoo_address.label.address_line1',
+                                                                   'class' => 'location'),
+                                                                   'property_path' => 'addressLine1'))
+            ->add('address_line_2', 'text', array(  'label' => array('value' => 'zizoo_address.label.address_line2',
+                                                                     'class' => 'location'),
+                                                                     'property_path' => 'addressLine2',
+                                                                     'required'  => false))
+            ->add('postcode', 'text', array('label' => array('value' => 'zizoo_address.label.postcode',
+                                                             'class' => 'postal_code'),
+                                                             'property_path' => 'postcode'))
+            ->add('locality', 'text', array('label' => array('value' => 'zizoo_address.label.locality',
+                                                             'class' => 'location'),
+                                                             'property_path' => 'locality'))
 //            ->add('sub_locality', 'text', array('required' => false,
 //                                                'label'     => array(   'value' => 'zizoo_address.label.sub_locality',
 //                                                                        'class' => 'location'),
 //                                                'property_path' => 'subLocality'))
-            ->add('country', 'entity', array(
-                                            'class' => 'ZizooAddressBundle:Country',
-                                            'query_builder' => function(EntityRepository $er) {
+            ->add('country', 'entity', array('class' => 'ZizooAddressBundle:Country',
+                                             'query_builder' => function(EntityRepository $er) {
                                                 return $er->createQueryBuilder('c')
-                                                    ->orderBy('c.order, c.printableName', 'ASC');
-                                            },
-                                            'label'     => array(   'value' => 'zizoo_address.label.country',
-                                                                    'class' => 'country'),
-                                            'property_path' => 'country'
-                                        ))
-            ->add('lat', 'hidden', array(   'required'  => false,
-                                            'read_only'  => true))
-            ->add('lng', 'hidden', array(   'required'  => false,
-                                            'read_only'  => true));
+                                                          ->orderBy('c.order, c.printableName', 'ASC');
+                                                },
+                                             'label'     => array('value' => 'zizoo_address.label.country',
+                                             'class' => 'country'),
+                                             'property_path' => 'country'
+                                            ))
+            ->add('lat', 'hidden', array('required'  => false,
+                                         'read_only'  => true))
+            ->add('lng', 'hidden', array('required'  => false,
+                                         'read_only'  => true));
                      
     }
 
