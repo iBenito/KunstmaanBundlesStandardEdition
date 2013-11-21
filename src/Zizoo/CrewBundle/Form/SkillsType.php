b4f2488f1ac07ae4c0f74d179ce9f1d58b65f05c
vbenitogo <vbenitogo@gmail.com>
2013-11-21 15:40:32 +0100

    #157

--- src/Zizoo/CrewBundle/Form/SkillsType.php
+++ src/Zizoo/CrewBundle/Form/SkillsType.php
@@ -34,7 +34,7 @@ class SkillsType extends AbstractType
                     'upload_error_js'   => 'licenseUploadError',
                     'upload_success_js' => 'licenseUploadSuccess',
                 )))->addViewTransformer($transformer))
-            ->add('experience')
+            ->add('experience', 'number', array('attr' => array('placeholder'=>'in years')))
             ->add('boats', 'entity', array(
                 'class' => 'ZizooBoatBundle:BoatType',
                 'multiple'  => true,
